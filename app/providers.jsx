d45aea83f4e0c58bb4ce0b93f5e675d82819efec
sleal-alt <s.leal@eveom.fr>
2026-06-28 18:16:08 +0200

    Remove base44 auth dependency from providers

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,7 +1,6 @@
 'use client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { AuthProvider } from '@/lib/AuthContext'
 import { Toaster } from '@/components/ui/toaster'
 import CookieBanner from '@/components/shared/CookieBanner'
 import Navbar from '@/components/layout/Navbar'
@@ -10,19 +9,17 @@ import StickyMobileCTA from '@/components/layout/StickyMobileCTA'
 
 export default function Providers({ children }) {
   return (
-    <AuthProvider>
-      <QueryClientProvider client={queryClientInstance}>
-        <div className="min-h-screen flex flex-col">
-          <Navbar />
-          <main className="flex-1 pb-16 lg:pb-0">
-            {children}
-          </main>
-          <Footer />
-          <StickyMobileCTA />
-        </div>
-        <Toaster />
-        <CookieBanner />
-      </QueryClientProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClientInstance}>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1 pb-16 lg:pb-0">
+          {children}
+        </main>
+        <Footer />
+        <StickyMobileCTA />
+      </div>
+      <Toaster />
+      <CookieBanner />
+    </QueryClientProvider>
   )
 }
