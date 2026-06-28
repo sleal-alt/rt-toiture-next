0596f4d3ca9d2b33332ce93c904087165008adb8
sleal-alt <s.leal@eveom.fr>
2026-06-28 19:07:14 +0200

    Debug: strip providers to bare minimum

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,25 +1,5 @@
 'use client'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClientInstance } from '@/lib/query-client'
-import { Toaster } from '@/components/ui/toaster'
-import CookieBanner from '@/components/shared/CookieBanner'
-import Navbar from '@/components/layout/Navbar'
-import Footer from '@/components/layout/Footer'
-import StickyMobileCTA from '@/components/layout/StickyMobileCTA'
 
 export default function Providers({ children }) {
-  return (
-    <QueryClientProvider client={queryClientInstance}>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1 pb-16 lg:pb-0">
-          {children}
-        </main>
-        <Footer />
-        <StickyMobileCTA />
-      </div>
-      <Toaster />
-      <CookieBanner />
-    </QueryClientProvider>
-  )
+  return <>{children}</>
 }
